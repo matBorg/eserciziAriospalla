aaf32096fbc399eb0ffd102a14fb150c30f1d77f
Matteo Borgesano <matteoborgesano@MacBook-Pro-di-Matteo.local>
2021-02-20 11:56:02 +0100

    Pronto alla verifica

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 const fetch = require("node-fetch")
 const process = require('process')
+const secrets = require("./secrets.js")
 // qui si possono vedere dal browser i risultati "192.168.1.231:8080/risultati/viewer"
-
-const credenziali = {
-        	nome: //"inserisci il tuo nome"
-        }
-
-
+const credenziali = secrets.name
+const ipAddress = secrets.ipAddress
 function accreditamento(obj){
-    fetch("192.168.1.231:8080/accreditamento", {
+    fetch(ipAddress+":8080/accreditamento", {
         method: 'post',
         body:    JSON.stringify(obj),
         headers: { 'Content-Type': 'application/json' },
@@ -17,33 +14,44 @@ function accreditamento(obj){
     .then( resBody => console.log(resBody))
     .catch(err => console.log(err))
 }
-//accreditamento(credenziali)
-function exercises(es, data){
-    case 1:
 
+accreditamento(credenziali)
+
+function exercises(es, data){
+    switch(es){
+        case 1:
+            console.log(data.toLowerCase())
+            return data.toLowerCase()
+            break;
+        default:
+            console.log("esercizio non trovato")
+    }
 }
 
 
-fetch("192.168.1.231:8080/esercizi/"+process.argv[2].toString(), {
-        method: 'get',
-        headers: { 'x-data': 'true' },
+fetch(ipAddress+":8080/esercizi/"+process.argv[2].toString(), {
+    method: 'get',
+    headers: { 'x-data': 'true' },
+})
+.then( res => res.json())
+.then( dati => {
+    console.log(dati)
+    console.log(dati.data)
+    return fetch(ipAddress+":8080/esercizi/"+process.argv[2].toString(), {
+        method: "post",
+        body: JSON.stringify({
+            data: exercises(+process.argv[2], dati.data)
+        }),
+        headers: {
+        "Content-Type": "application/json"
+        },
     })
     .then( res => res.json())
     .then( resBody => console.log(resBody))
-    .then( dati => {
-        fetch("192.168.1.231:8080/esercizi/"+process.argv[2].toString(), {
-            method: "post",
-            body: JSON.stringify({
-                data: exercises(+process.argv[2], dati)
-            }),
-            headers: {
-            "Content-Type": "application/json"
-            },
-        })
-        .then( res => res.json())
-        .then( resBody => console.log(resBody))
-        .catch(err => console.log(err))
-    }
     .catch(err => console.log(err))
+})
+.catch(err => console.log(err))
+
+
 
 
